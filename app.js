75cfeff15fe0e067ec09f56b8a37884f05fabc27
Sara Torrey <sara.moravej@gmail.com>
2019-06-30 21:52:26 -0500

    Fix syntax error

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-let express     = require( "express" );
-let app         = express();
-let bodyParser  = require( "body-parser" );
-let mongoose    = require ("mongoose");
+let express = require( "express" );
+let app = express();
+let bodyParser = require( "body-parser" );
+let mongoose = require( "mongoose" );
 
 mongoose.connect( "mongodb://localhost:27017/star_camp", {useNewUrlParser: true} );
-app.use(bodyParser.urlencoded({extended: true}));
+app.use( bodyParser.urlencoded( {extended: true} ) );
 app.set( "view engine", "ejs" );
 
 //Schema setup
-let campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String,
-    });
+let campgroundSchema = new mongoose.Schema( {
+                                                name: String,
+                                                image: String,
+                                                description: String,
+                                            } );
 
 let Campground = mongoose.model( "Campground", campgroundSchema );
 
@@ -31,27 +31,26 @@ let Campground = mongoose.model( "Campground", campgroundSchema );
 //        }
 //     });
 
-
 app.get( "/", function ( req, res ) {
-    res.render("landing");
+    res.render( "landing" );
 } );
 
 //INDEX route
 app.get( "/campgrounds", function ( req, res ) {
-    Campground.find({}, function ( err, allCampgrounds ) {
-        if(err) {
-            console.log(err);
-        } else {
+    Campground.find( {}, function ( err, allCampgrounds ) {
+        if ( err ) {
+            console.log( err );
+        }
+        else {
             res.render( "index", {campgrounds: allCampgrounds} );
         }
-    });
+    } );
 } );
 
 //NEW route
-app.get( "/campgrounds/new",function (req, res) {
+app.get( "/campgrounds/new", function ( req, res ) {
     res.render( "new" );
 } );
-
 
 // CREATE route
 app.post( "/campgrounds", function ( req, res ) {
@@ -60,25 +59,28 @@ app.post( "/campgrounds", function ( req, res ) {
     let image = req.body.image;
     let newCampground = {name: name, image: image};
     //create a new campground & save to DB
-    Campground.create(newCampground, function ( err, newlyCampground ) {
-        if(err){
-            console.log(err);
-        }else {
-            res.redirect("/campgrounds");
+    Campground.create( newCampground, function ( err, newlyCampground ) {
+        if ( err ) {
+            console.log( err );
         }
-    });
+        else {
+            res.redirect( "/campgrounds" );
+        }
+    } );
 } );
 
 //Show template
-app.get("/campgrounds/:id", function ( req, res ) {
-    Campground.findById(req.params.id, function ( err, foundCampground ) {
-        if(err){
-            console.log(err);
-        }else {
-            res.render("show", {campgound: foundCampground});
+app.get( "/campgrounds/:id", function ( req, res ) {
+    Campground.findById( req.params.id, function ( err, foundCampground ) {
+        if ( err ) {
+            console.log( err );
         }
-});
+        else {
+            res.render( "show", {campgound: foundCampground} );
+        }
+    } );
+} );
 
-app.listen(3000, process.env.IP, function (  ) {
-    console.log("star-camp has started!")
-});
+app.listen( 3000, process.env.IP, function () {
+    console.log( "star-camp has started!" );
+} );
